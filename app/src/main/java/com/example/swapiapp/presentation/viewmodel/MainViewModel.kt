1fd016ec12fa1791235df8d7c0e4dbae258f5f03
Artem <adpm@list.ru>
2023-06-25 19:44:02 +0600

    catch network errors

--- app/src/main/java/com/example/swapiapp/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/swapiapp/presentation/viewmodel/MainViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.swapiapp.presentation.viewmodel
 
+import android.accounts.NetworkErrorException
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.swapiapp.data.storage.network.GenericErrorException
 import com.example.swapiapp.domain.usecase.GetPeopleByName
 import com.example.swapiapp.domain.usecase.GetStarshipsByName
 import com.example.swapiapp.presentation.state.ViewState
@@ -23,14 +26,24 @@ class MainViewModel(
 
     fun getPeople(name: String) {
         viewModelScope.launch {
-            val people = getPeopleByName(name)
-            val starships = getStarshipsByName(name)
+            _viewState.update { currentState -> currentState.copy(loading = true) }
+            try {
+                val people = getPeopleByName(name)
+                val starships = getStarshipsByName(name)
 
-            _viewState.update { currentState ->
-                currentState.copy(
-                    people = people,
-                    starships = starships
-                )
+                _viewState.update { currentState ->
+                    currentState.copy(
+                        loading = false,
+                        people = people,
+                        starships = starships
+                    )
+                }
+            } catch (e: NetworkErrorException) {
+                _viewState.update { currentState -> currentState.copy(isNoNetworkError = true) }
+                Log.d("TAG_VM", "error : ${e.message}")
+            } catch (e: GenericErrorException) {
+                _viewState.update { currentState -> currentState.copy(isServerError  = true) }
+                Log.d("TAG_VM", "error : ${e.message}")
             }
         }
     }
